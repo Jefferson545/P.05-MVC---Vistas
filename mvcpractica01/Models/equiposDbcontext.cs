accde1935a8007e21f47d8418583d5b97a5c7e4b
Jefferson545 <jefferson.avelar@catolica.edu.sv>
2023-03-31 23:56:15 -0600

    dbcontext

--- mvcpractica01/Models/equiposDbcontext.cs
+++ mvcpractica01/Models/equiposDbcontext.cs
@@ -9,12 +9,12 @@ namespace mvcpractica01.Models
         {
         }
         public DbSet<marcas> marcas { get; set; }
-        public DbSet<webApipractica.Models.estados_equipo>? estados_equipo { get; set; }
-        public DbSet<webApipractica.Models.estados_reserva>? estados_reserva { get; set; }
-        public DbSet<webApipractica.Models.facultades>? facultades { get; set; }
-        public DbSet<webApipractica.Models.tipo_equipo>? tipo_equipo { get; set; }
-        public DbSet<webApipractica.Models.usuarios>? usuarios { get; set; }
-        public DbSet<webApipractica.Models.carreras>? carreras { get; set; }
+        public DbSet<estados_equipo>? estados_equipo { get; set; }
+        public DbSet<estados_reserva>? estados_reserva { get; set; }
+        public DbSet<facultades>? facultades { get; set; }
+        public DbSet<tipo_equipo>? tipo_equipo { get; set; }
+        public DbSet<usuarios>? usuarios { get; set; }
+        public DbSet<carreras>? carreras { get; set; }
 
     }
 }
