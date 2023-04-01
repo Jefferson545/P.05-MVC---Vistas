d1c469dc3f6f86808e4ae1389ced2b841b68de3c
jefferson <jefferson.avelar@catolica.edu.sv>
2023-03-31 22:24:01 -0600

    Agregar archivos de proyecto.

---
+++ mvcpractica01/Models/equiposDbcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using webApipractica.Models;
+namespace mvcpractica01.Models
+{
+    public class equiposDbcontext : DbContext
+    {
+        public equiposDbcontext(DbContextOptions<equiposDbcontext> options) : base(options)
+        {
+        }
+        public DbSet<marcas> marcas { get; set; }
+        public DbSet<webApipractica.Models.estados_equipo>? estados_equipo { get; set; }
+
+    }
+}
